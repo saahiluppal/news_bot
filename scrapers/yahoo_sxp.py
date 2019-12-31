1b765d46914b1a48b9c7694c1de410857f87730a
Sahil Uppal <sahiluppal2k@gmail.com>
2019-12-31 20:02:03 +0530

    done with headlines

--- scrapers/yahoo_sxp.py
+++ scrapers/yahoo_sxp.py
@@ -1,6 +1,12 @@
 def headlines(browser):
     browser.get('https://news.yahoo.com')
-    head = browser.find_element_by_xpath('//h2[@data-reactid]').text
-    desc = browser.find_element_by_xpath('//p[@data-reactid]').text
-    print(head)
-    print(desc)
+    headline = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div[1]/div/div[2]/div/div/div/ul/li[1]/div/div/a/div[3]/div/h2').text
+    description = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div[1]/div/div[2]/div/div/div/ul/li[1]/div/div/a/div[3]/div/div/p').text
+    img_src = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div[1]/div/div[2]/div/div/div/ul/li[1]/div/div/a/img').get_attribute('src')
+    news_page = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div[1]/div/div[2]/div/div/div/ul/li[1]/div/div/a').get_attribute('href')
+
+    return [headline, description, img_src, news_page]
+
+def trending(browser):
+    headlines = list()
+    
