7a7726414f3cace18300f9fccb33c1012ce3d7e2
Sahil Uppal <sahiluppal2k@gmail.com>
2020-01-01 16:13:52 +0530

    done with headlines

--- scrapers/yahoo_sxp.py
+++ scrapers/yahoo_sxp.py
@@ -1,12 +1,12 @@
 def headlines(browser):
     browser.get('https://news.yahoo.com')
-    headline = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div[1]/div/div[2]/div/div/div/ul/li[1]/div/div/a/div[3]/div/h2').text
-    description = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div[1]/div/div[2]/div/div/div/ul/li[1]/div/div/a/div[3]/div/div/p').text
-    img_src = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div[1]/div/div[2]/div/div/div/ul/li[1]/div/div/a/img').get_attribute('src')
-    news_page = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div[1]/div/div[2]/div/div/div/ul/li[1]/div/div/a').get_attribute('href')
+
+    headline_path = '/html/body/div[1]/div/div[1]/div/div[3]/div[1]/div/div[2]/div/div/div/ul/li[1]/div/div/a'
+
+    headline = browser.find_element_by_xpath(headline_path + '/div[3]/div/h2').text
+    description = browser.find_element_by_xpath(headline_path + '/div[3]/div/div/p').text
+    img_src = browser.find_element_by_xpath(headline_path + '/img').get_attribute('src')
+    news_page = browser.find_element_by_xpath(headline_path).get_attribute('href')
 
     return [headline, description, img_src, news_page]
 
-def trending(browser):
-    headlines = list()
-    
