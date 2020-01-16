c7da150156ae1d100ab812330c60c077f287f287
Sahil Uppal <sahiluppal2k@gmail.com>
2020-01-16 18:35:26 +0530

    auto close attribute on main

--- scrapers/sxp.py
+++ scrapers/sxp.py
@@ -3,5 +3,5 @@ import yahoo_sxp as yahoo
 import os
 
 browser = webdriver.Firefox(executable_path=os.getcwd()+'/geckodriver')
-print(yahoo.fetch(browser, ['trending']))
-#browser.close()
+print(yahoo.fetch(browser, ['world']))
+browser.close()
