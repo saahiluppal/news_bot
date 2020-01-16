a28262c935db6de810e58d9c42806f13a5200e87
Sahil Uppal <sahiluppal2k@gmail.com>
2020-01-16 18:36:13 +0530

    world added

--- scrapers/yahoo_sxp.py
+++ scrapers/yahoo_sxp.py
@@ -4,7 +4,8 @@ def fetch(browser, entities=['headlines'], count=10):
 
     for entity in entities:
         if entity is 'headlines': returnable.append(headlines(browser))
-        if entity is 'trending': returnable.append(trending(browser, count))
+        if entity is 'trending': returnable.append(trending(browser, 5))
+        if entity is 'world': returnable.append(world(browser, count))
 
     return returnable
 
@@ -26,6 +27,8 @@ def headlines(browser):
 def trending(browser, count):
     browser.get('https://news.yahoo.com')
 
+    print('Implement it again')
+
     trending_path = '/html/body/div[1]/div/div[1]/div/div[3]/div[1]/div/div[2]/div/div/div/ul'
     browser.implicitly_wait(1)
 
@@ -40,3 +43,33 @@ def trending(browser, count):
 
     return [None, None, None, None]
 
+def world(browser, count):
+    browser.get('https://news.yahoo.com/world/')
+
+    path = '/html/body/div[1]/div/div[1]/div/div[3]/div[1]/div/div[2]/div/div/div/ul'
+    headlines = []
+    descriptions = []
+    img_srcs = []
+    urls = []
+    counter = 0
+
+    for i in range(100):
+        try:
+            headline = browser.find_element_by_xpath(path + f'/li[{i}]/div/div/div[2]/h3/a').text
+            description = browser.find_element_by_xpath(path + f'/li[{i}]/div/div/div[2]/p').text
+            img_src = browser.find_element_by_xpath(path + f'/li[{i}]/div/div/div[1]/div/img').get_attribute('src')
+            url = browser.find_element_by_xpath(path + f'/li[{i}]/div/div/div[2]/h3/a').get_attribute('href')
+            print(url)
+            headlines.append(headline)
+            descriptions.append(description)
+            img_srcs.append(img_src)
+            url.append(url)
+            counter+=1
+            if counter >= count:
+                break
+            print('hello world')
+        except:
+            pass
+
+        print(headlines, descriptions, img_srcs, urls)
+        return [headlines, descriptions, img_srcs, urls]
